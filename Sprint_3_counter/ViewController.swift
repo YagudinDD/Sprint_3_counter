dd4dcb0f37ad95456e1a57394c08dc258982a393
YagudinDD <ddyagudin@yandex.ru>
2022-11-09 20:36:14 +0300

    Внес изменения по комментариям: 1. Изменил приватность 2. Поправил название функции  3. Сделал отступ

--- Sprint_3_counter/ViewController.swift
+++ Sprint_3_counter/ViewController.swift
@@ -9,25 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-@IBOutlet weak var userScoreLabel: UILabel!
-//  значение счета
-@IBOutlet var buttonScore: UIView!
-//  кнопка для увеличения счета
-override func viewDidLoad() {
-    super.viewDidLoad()
-    userScoreLabel.text = ("Значение счетчика:\(userScore)")
-}
-// описал начальный экран запуска
-var userScore: Int = 0
-//задал значение переменной которая является счетчиком
-private func updateUserScoreLabel() {
-    userScoreLabel.text = "\(userScore)"
-}
-// функция в которой отображаем значение в основной программе
-@IBAction func ButtonScoreLabel(_ sender: Any) {
-    userScore += 1
-    userScoreLabel.text = "Значение счетчика:\(userScore)"
-
-}
-// кнопка счетчик обнавляющая значение
+    @IBOutlet private weak var userScoreLabel: UILabel!
+    //  значение счета
+    @IBOutlet private var buttonScore: UIView!
+    //  кнопка для увеличения счета
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        userScoreLabel.text = ("Значение счетчика:\(userScore)")
+    }
+    // описал начальный экран запуска
+    private var userScore: Int = 0
+    //задал значение переменной которая является счетчиком
+   private func updateUserScoreLabel() {
+        userScoreLabel.text = "\(userScore)"
+    }
+    // функция в которой отображаем значение в основной программе
+    @IBAction private func buttonScoreLabel(_ sender: Any) {
+        userScore += 1
+        userScoreLabel.text = "Значение счетчика:\(userScore)"
+    }
+    // кнопка счетчик обнавляющая значение
 }
